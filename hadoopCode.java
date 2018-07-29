d071959e64712925bb52d2daa00db144ec4c3748
Aadi <adi@gmail.com>
2018-07-30 05:04:58 +0530

    intial commit

---
+++ hadoopCode.java
@@ -0,0 +1,60 @@
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+hadoopCode.java [unix] (05:29 01/01/1970)                              0,0-1 All
+-- VISUAL BLOCK --                                                   1x1
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+rerererrrr~
+~
+~
+~
+hadoopCode.java [unix] (05:29 01/01/1970)                              0,0-1 All
+-- VISUAL BLOCK --                                                   1x1
+Hadoop
+
+1. Storage layer - HDFS (hadoop distributed file system)
+
+Name node - Master Daemon (Receptionist)
+Data node - Slave daemon (hotel rooms)
+Secondary name node 
+
+2. Processing layer - Mapreduce (framework to allow distributed and parallel computing)
+
+Job Tracker - Master Daemon
+Task tracker - Slave daemon
